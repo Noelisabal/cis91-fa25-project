97eb444c1946d0c93ed4fb29573a02f723750bb8
Noel Hernandez <nelo.isabal@gmail.com>
2025-12-12 03:58:19 +0000

    added edit to the crontab

--- backupscript.sh
+++ backupscript.sh
@@ -4,4 +4,4 @@ sudo systemctl stop mysql
 sudo tar -cf /tmp/db-backup.tar mysql/
 sudo systemctl start mysql
 gsutil cp /tmp/db-backup.tar gs://cis91fa25noel-backup-bucket
-rm /tmp/db-backup.tar
+sudo rm /tmp/db-backup.tar
